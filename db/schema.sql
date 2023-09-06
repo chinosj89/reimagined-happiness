727f8f7aa5c0355a0100aa64e4ba310233019e19
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-09-05 17:11:43 -0700

    finished schema

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL, 
+    department_id INT,
+    -- to hold reference to department role belongs to --
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    -- to hold reference to employee role --
+    FOREIGN KEY(role_id),
+    REFERENCES role(id),
+    ON DELETE SET NULL 
+    -- to hold reference to another employee that is the manager of the current employee (null if the employee has no manager) --
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id),
+    ON DELETE SET NULL 
+)
+
+
