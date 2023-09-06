8e369038bd20a0c218270c9c6d2449990b896cbe
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-09-05 17:00:54 -0700

    adding db and finished prompts

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
 // Import and require inquirer 
-const inquirer = require('inquirer');
+const choices = inquirer.prompt(questions);
+// to hide password - learned in module 13
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,9 +16,9 @@ app.use(express.json());
 // mySQL connection
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: '',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
       database: 'employees_db'
     },
     console.log(`Connected to the employees database.`)
